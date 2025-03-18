05e188ba5419cec2154dd8387726d60e52cdda06
Steven Wood <steve@coloradowoods.com>
2025-03-18 14:43:06 -0600

    Added Keypad logic

--- src/main.py
+++ src/main.py
@@ -243,6 +243,29 @@ async def main():
     refresh(ssd, True)
     # PID loop - Default behaviour
     powerup = True
+
+    # Create a map between keypad buttons and characters
+    matrix_keys = [['1', '2', '3', 'A'],
+               ['4', '5', '6', 'B'],
+               ['7', '8', '9', 'C'],
+               ['*', '0', '#', 'D']]
+
+    # PINs according to schematic - Change the pins to match with your connections
+    keypad_rows = [15,14,13,12]
+    keypad_columns = [11,10,9,8]
+
+    # Create two empty lists to set up pins ( Rows output and columns input )
+    col_pins = []
+    row_pins = []
+
+    # Loop to assign GPIO pins and setup input and outputs
+    for x in range(0,4):
+        row_pins.append(Pin(keypad_rows[x], Pin.OUT))
+        row_pins[x].value(1)
+        col_pins.append(Pin(keypad_columns[x], Pin.IN, Pin.PULL_DOWN))
+        col_pins[x].value(0)
+
+
     while True:
         if powerup:
             if started == True:
@@ -270,7 +293,18 @@ async def main():
                 if dt > checkin:
                     utime.sleep(.1)
                     lastupdate = now
-
+                # Scan Keys
+                for row in range(4):
+                    for col in range(4):
+                        row_pins[row].high()
+                        key = None
+                        
+                        if col_pins[col].value() == 1:
+                            print("You have pressed:", matrix_keys[row][col])
+                            key_press = matrix_keys[row][col]
+                            utime.sleep(0.3)
+                                
+                    row_pins[row].low()
             except Exception as e:
                 # Put something to output to OLED screen
                 beanaproblem('error.')
